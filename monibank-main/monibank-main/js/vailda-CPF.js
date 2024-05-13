28300053127856f3871e62d6388f7c816177f592
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-05-13 15:35:58 -0300

    finalização do projeto

--- monibank-main/monibank-main/js/vailda-CPF.js
+++ monibank-main/monibank-main/js/vailda-CPF.js
@@ -1,6 +1,13 @@
 export default function eUmCPF(campo) {
-    const cpf = campo.value.replace(/\.|-/g, '')
-    validaNumerosRepetidos(cpf);
+    const cpf = campo.value.replace(/\.|-/g, '');
+
+    if (validaNumerosRepetidos || validaPrimeiroDigito || validaSegundoDigito) {
+        campo.setCustomValidity('Este CPF não é válido')
+    }
+
+    else{
+        console.log('Este CPF existe!')
+    }
 }
 
 function validaNumerosRepetidos (cpf) {
@@ -18,4 +25,40 @@ function validaNumerosRepetidos (cpf) {
     ]
 
     return numerosRepetidos.includes(cpf)
+}
+
+function validaPrimeiroDigito (cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho ++){
+        soma += cpf[tamanho] * multiplicador
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11
+
+    if (soma == 10 || soma == 11 ){
+        soma ==0
+    }
+
+    return soma != cpf[9]
+}
+
+function validaSegundoDigito (cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho ++){
+        soma += cpf[tamanho] * multiplicador
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11
+
+    if (soma == 10 || soma == 11 ){
+        soma ==0
+    }
+
+    return soma != cpf[10]
 }
